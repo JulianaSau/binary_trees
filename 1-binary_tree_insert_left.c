79bb5cb9ac63bb0ee334ac41afd2c077bbd9e67a
Juliana Sau <julienyange@gmail.com>
2022-08-23 05:24:51 -0700

    fix bug

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,7 @@
  * @parent:  pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
- * Return: pointer to the created node,
- * 		NULL on failure
- * 		NULL if parent is NULL
+ * Return: pointer to the created node, NULL on failure, NULL if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -30,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left == NULL)
 	{
-		parent->left = new_node
+		parent->left = new_node;
 	}
 	else
 	{
