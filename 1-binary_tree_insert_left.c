c9182a2d8af11e8dee2334118103c8926c3c4dc8
Juliana Sau <julienyange@gmail.com>
2022-08-23 05:53:39 -0700

    fix betty style errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,10 +2,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * binary_tree_insert_left - function that inserts a node as
+ * the left-child of another node
  *
- * Description: if parent already has a left-child, the new node must take its place
- * 		and the old left-child must be set as the left-child of the new node.
+ * Description: if parent already has a left-child, the new
+ * node must take its place and the old left-child must be
+ * set as the left-child of the new node.
  *
  * @parent:  pointer to the node to insert the left-child in
  * @value: value to store in the new node
@@ -15,7 +17,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 	{
 		return (NULL);
