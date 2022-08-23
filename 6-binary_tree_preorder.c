e4af98457293c99e423e1044bd54a5a0d3dd258a
Juliana Sau <julienyange@gmail.com>
2022-08-23 07:15:18 -0700

    fix betty style error

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,9 +7,10 @@
  *
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
- * 		the value in the node must be passed as a param to this function
+ * the value in the node must be passed as a param to this function
+ *
  * Return: nothing
- * 		if tree or func is null do nothing
+ * if tree or func is null do nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
